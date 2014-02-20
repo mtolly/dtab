9d4407cc4b55fd3b615357c331f86a25f30f1994
Michael Tolly <onyxite@gmail.com>
2014-02-20 15:52:08 -0600

    some cleanup and docs

--- src/Data/DTA/Serialize.hs
+++ src/Data/DTA/Serialize.hs
@@ -14,9 +14,12 @@ unserialize (DTA _ (Tree _ cs)) = fromChunks cs
 serialize :: (ToChunks a) => a -> DTA
 serialize = DTA 0 . Tree 0 . toChunks
 
+-- | Values which are stored as one or many chunks. Scalar types become one
+-- chunk, while lists and record types can make more.
 class ToChunks a where
   toChunks :: a -> [Chunk]
 
+-- | Values which can be read from one or many chunks.
 class FromChunks a where
   fromChunks :: [Chunk] -> Either String a
 
@@ -58,9 +61,8 @@ dictLookup k (Dict m) = case Map.lookup k m of
   Nothing -> Left $ "Couldn't find key " ++ show k
   Just v  -> Right v
 
-makeDict' :: [(B8.ByteString, [Chunk])] -> [Chunk]
-makeDict' = makeDict . Dict . Map.fromList
-
+-- | A value which is DTA-stored as a parenthesized subtree around the normal
+-- representation.
 newtype InParens a = InParens { fromInParens :: a }
   deriving (Eq, Ord, Show, Read)
 
@@ -71,10 +73,12 @@ instance (FromChunks a) => FromChunks (InParens a) where
   fromChunks [Parens (Tree _ cs)] = fmap InParens $ fromChunks cs
   fromChunks cs = Left $ "Couldn't read as InParens: " ++ show cs
 
+-- | An integer 0 or 1.
 instance ToChunks Bool where
   toChunks True = [Int 1]
   toChunks False = [Int 0]
 
+-- | An integer 0 or 1.
 instance FromChunks Bool where
   fromChunks [Int 1] = Right True
   fromChunks [Int 0] = Right False
@@ -94,34 +98,43 @@ instance FromChunks Float where
   fromChunks [Float f] = Right f
   fromChunks cs = Left $ "Couldn't read as Float: " ++ show cs
 
+-- | A String, not a 'Key'.
 instance ToChunks B8.ByteString where
   toChunks bs = [String bs]
 
+-- | A String, not a 'Key'.
 instance FromChunks B8.ByteString where
   fromChunks [String bs] = Right bs
   fromChunks cs = Left $ "Couldn't read as ByteString: " ++ show cs
 
+-- | Stored as two chunks. Each subtype should be a single chunk.
 instance (ToChunks a, ToChunks b) => ToChunks (a, b) where
   toChunks (x, y) = toChunks x ++ toChunks y
 
+-- | Stored as two chunks. Each subtype should be a single chunk.
 instance (FromChunks a, FromChunks b) => FromChunks (a, b) where
   fromChunks [x, y] = liftA2 (,) (fromChunks [x]) (fromChunks [y])
   fromChunks cs = Left $ "Couldn't read as pair: " ++ show cs
 
+-- | Represents 'Nothing' with an empty chunk list.
 instance (ToChunks a) => ToChunks (Maybe a) where
   toChunks Nothing = []
   toChunks (Just x) = toChunks x
 
+-- | Represents 'Nothing' with an empty chunk list.
 instance (FromChunks a) => FromChunks (Maybe a) where
   fromChunks [] = Right Nothing
   fromChunks cs = fmap Just $ fromChunks cs
 
+-- | Each value is stored as one chunk in the list.
 instance (ToChunks a) => ToChunks [a] where
   toChunks = concatMap toChunks
 
+-- | Each value is stored as one chunk in the list.
 instance (FromChunks a) => FromChunks [a] where
   fromChunks = mapM $ \x -> fromChunks [x]
 
+-- | Stored as a 'Key', unlike the 'ByteString' instance which is a String.
 newtype Keyword = Keyword { fromKeyword :: B8.ByteString }
   deriving (Eq, Ord, Show, Read)
 
@@ -132,9 +145,11 @@ instance FromChunks Keyword where
   fromChunks [Key k] = Right $ Keyword k
   fromChunks cs = Left $ "Couldn't read as Keyword: " ++ show cs
 
+-- | Uses whichever 'toChunks' is applicable. Does not tag which type is used.
 instance (ToChunks a, ToChunks b) => ToChunks (Either a b) where
   toChunks = either toChunks toChunks
 
+-- | First tries to read the 'Left' type, then 'Right' if that fails.
 instance (FromChunks a, FromChunks b) => FromChunks (Either a b) where
   fromChunks cs = case (fromChunks cs, fromChunks cs) of
     (Right l, _      ) -> Right $ Left l
